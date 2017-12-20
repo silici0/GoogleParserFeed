ad0a107109394825ec70696f8708b65a3d203033
Rafael Cunha <silici0@gmail.com>
2017-12-20 11:31:52 -0200

    Working project

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # GoogleParserFeed
+
+
+
+# splio api access via PHP
+
 Parse google merchant feed to array  PHP
+
+## Installation
+
+```
+composer required silici0/googleparserfeed:dev-master
+```
+
+
+## Usage example
+
+```
+require "vendor/autoload.php";
+use silici0\GoogleParserFeed\ParserGoogleFeed;
+
+$buffer = file_get_contents("http://www.domain.com/googleshopping.xml");
+
+$feed = new ParserGoogleFeed($buffer, LIBXML_NOCDATA);
+print_r($feed->toArray());
+
+```
+
+
+## Contribution
+
+https://stackoverflow.com/questions/26400993/resolve-namespaces-with-simplexml-regardless-of-structure-or-namespace
+https://hakre.wordpress.com/2013/07/10/simplexml-and-json-encode-in-php-part-iii-and-end/
---
+++ src/ParserGoogleFeed.php
@@ -0,0 +1,54 @@
+<?php namespace silici0\GoogleParserFeed;
+
+use SimpleXMLElement;
+use JsonSerializable;
+
+class ParserGoogleFeed extends SimpleXMLElement implements JsonSerializable 
+{
+
+    public function jsonSerialize()
+    {
+        $array = array();
+
+        // json encode attributes if any.
+        if ($attributes = $this->attributes()) {
+            $array['@attributes'] = iterator_to_array($attributes);
+        }
+
+        $namespaces = [null] + $this->getDocNamespaces(true);
+        // json encode child elements if any. group on duplicate names as an array.
+        foreach ($namespaces as $namespace) {
+            foreach ($this->children($namespace) as $name => $element) {
+                if (isset($array[$name])) {
+                    if (!is_array($array[$name])) {
+                        $array[$name] = [$array[$name]];
+                    }
+                    $array[$name][] = $element;
+                } else {
+                    $array[$name] = $element;
+                }
+            }
+        }
+
+        // json encode non-whitespace element simplexml text values.
+        $text = trim($this);
+        if (strlen($text)) {
+            if ($array) {
+                $array['@text'] = $text;
+            } else {
+                $array = $text;
+            }
+        }
+
+        // return empty elements as NULL (self-closing or empty tags)
+        if (!$array) {
+            $array = NULL;
+        }
+
+        return $array;
+    }
+
+    public function toArray() {
+        return (array) json_decode(json_encode($this));
+    }
+}
